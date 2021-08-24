a06456f212c35c11110b1771f43d1cf3a4002445
wqdq <asd>
2021-08-24 21:42:24 +0700

    add lib nextjs-progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { useStore } from "../store";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
+import NextNProgress from "nextjs-progressbar";
 import "style/style.css";
 
 export default function App({ Component, pageProps }) {
@@ -19,6 +20,7 @@ export default function App({ Component, pageProps }) {
           content="width=device-width,initial-scale=1,user-scalable=no"
         />
       </Head>
+      <NextNProgress height={4} color="#eb362d" />
       <Component {...pageProps} />
     </Provider>
   );
